88e92dda3a5ecb2e762983410402ef092a047bb8
MelanX <info@melanx.de>
2022-10-21 17:02:06 +0200

    fix issues with tools cannot be repaired
    closes #5

--- src/main/java/de/melanx/simplytools/EventHandler.java
+++ src/main/java/de/melanx/simplytools/EventHandler.java
@@ -3,40 +3,11 @@ package de.melanx.simplytools;
 import de.melanx.simplytools.items.BaseTool;
 import de.melanx.simplytools.util.BlockBreaker;
 import net.minecraft.server.level.ServerPlayer;
-import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.block.state.BlockState;
-import net.minecraftforge.event.AnvilUpdateEvent;
 import net.minecraftforge.event.level.BlockEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 
 public class EventHandler {
-
-    @SubscribeEvent
-    public void onAnvilUpdate(AnvilUpdateEvent event) {
-        if (event.getLeft().getItem() instanceof BaseTool) {
-            ItemStack addition = event.getRight();
-            ItemStack output = event.getLeft().copy();
-            if (!output.getItem().isValidRepairItem(event.getLeft(), addition)) {
-                return;
-            }
-
-            int damageReduction = Math.min(output.getDamageValue(), output.getMaxDamage() / 7);
-            if (damageReduction <= 0) {
-                event.setOutput(ItemStack.EMPTY);
-                event.setCost(0);
-                return;
-            }
-
-            for (int i = 0; damageReduction > 0 && i < event.getRight().getCount(); ++i) {
-                int durability = output.getDamageValue() - damageReduction;
-                output.setDamageValue(durability);
-
-                damageReduction = Math.min(output.getDamageValue(), output.getMaxDamage() / 7);
-            }
-
-            event.setOutput(output);
-        }
-    }
 
     @SubscribeEvent
     public void onBlockBreak(BlockEvent.BreakEvent event) {
